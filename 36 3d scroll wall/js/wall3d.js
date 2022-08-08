7e88f1a2bd0b6c0294da3178f8e9ece00675fad3
bopboy <39391891+bopboy@users.noreply.github.com>
2022-08-08 23:05:02 +0900

    45 3d scroll wall 4 (progress bar)

--- 36 3d scroll wall/js/wall3d.js
+++ 36 3d scroll wall/js/wall3d.js
@@ -1,13 +1,16 @@
 (function () {
     const houseElem = document.querySelector('.house')
+    const barElem = document.querySelector('.progress-bar')
     let maxScrollValue
     function resizeHandler() {
         maxScrollValue = document.body.offsetHeight - window.innerHeight
     }
     window.addEventListener('resize', resizeHandler)
     window.addEventListener('scroll', function () {
-        const zMove = pageYOffset / maxScrollValue * 980 - 490
+        const scrollPer = pageYOffset / maxScrollValue
+        const zMove = scrollPer * 980 - 490
         houseElem.style.transform = `translateZ(${zMove}vw)`
+        barElem.style.width = scrollPer * 100 + "%"
     })
     resizeHandler()
 })()
