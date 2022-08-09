7be8f2d310a62f7db8d18053e193c483c45e288a
bopboy <39391891+bopboy@users.noreply.github.com>
2022-08-09 20:05:53 +0900

    46 3d scroll wall 5 (시점 변경)

--- 36 3d scroll wall/js/wall3d.js
+++ 36 3d scroll wall/js/wall3d.js
@@ -1,7 +1,10 @@
 (function () {
     const houseElem = document.querySelector('.house')
     const barElem = document.querySelector('.progress-bar')
+    const stageElem = document.querySelector('.stage')
     let maxScrollValue
+    const mousePos = { x: 0, y: 0 }
+
     function resizeHandler() {
         maxScrollValue = document.body.offsetHeight - window.innerHeight
     }
@@ -13,4 +16,11 @@
         barElem.style.width = scrollPer * 100 + "%"
     })
     resizeHandler()
+    window.addEventListener('mousemove', function (e) {
+        // console.log(e.clientX, e.clientY)
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2
+        // console.log(mousePos)
+        stageElem.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${mousePos.x * 5}deg)`
+    })
 })()
